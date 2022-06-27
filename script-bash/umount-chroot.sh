4d47f4480345f2b971b727633cc3617da72212c5
dragomirdanut <dragomirdanut@gmail.com>
2022-06-27 15:49:12 +0300

    Skip for Btrfs pool

--- script-bash/umount-chroot.sh
+++ script-bash/umount-chroot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# target_dir="/mnt/ubuntu3"
-target_dir=$1
+# TARGET_DIR="/mnt/ubuntu3"
+TARGET_DIR=$1
 
 # condition for param
 if [[ $# -le 0 ]]; then
@@ -9,12 +9,10 @@ if [[ $# -le 0 ]]; then
 	exit 0
 fi
 
-echo -e "Unmounting ${target_dir}"
+echo -e "Unmounting ${TARGET_DIR} ..."
 
-# If mnt_params contain "subvolid" = skip below! exit 0 (no error)
-if [[ ! $mnt_params =~ "btrfs" ]]; then
-	# invert orders
-	for i in ${target_dir}/sys/firmware/efi/efivars ${target_dir}/sys ${target_dir}/run ${target_dir}/proc ${target_dir}/dev/pts ${target_dir}/dev; 
+if [[ ! ${TARGET_DIR} =~ "btrfs" ]]; then
+	for i in ${TARGET_DIR}/sys/firmware/efi/efivars ${TARGET_DIR}/sys ${TARGET_DIR}/run ${TARGET_DIR}/proc ${TARGET_DIR}/dev/pts ${TARGET_DIR}/dev; 
 	do
 		echo " >> Umounting $i"
 		sudo umount $i
@@ -22,8 +20,8 @@ if [[ ! $mnt_params =~ "btrfs" ]]; then
 	echo -e "Done."
 fi
 
-echo -e "Final umount the target directory: ${target_dir}"
-sudo umount ${target_dir}
+echo -e "Final umount the target directory: ${TARGET_DIR}"
+sudo umount ${TARGET_DIR}
 echo -e "Done."
 
 exit 0
